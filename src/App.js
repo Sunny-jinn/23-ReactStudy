0d80758cc0a3e74c3722e754424d75090e536ecb
Sunny-jinn <rlawlsdn316@gmail.com>
2023-03-28 02:15:10 +0900

    week 1: Init project setting & create header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./assets/css/header.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
